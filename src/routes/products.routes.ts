38267717fe7703a35d160f7f6c9d52d386b54bad
João Paulo <jpborella1@gmail.com>
2026-04-09 22:35:52 -0300

    adiciona middleware de autenticação JWT e integra ao gerenciamento de produtos

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,6 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { listProducts } from "../controllers/products.controller";
+import { authenticate } from "../middlewares/auth.middlewares";
 
 export default async function productsRoutes(fastify: FastifyInstance) {
+    fastify.addHook("onRequest", authenticate);
     fastify.get("/", listProducts);
-}
+    };
+    
